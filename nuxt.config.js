a077c0be66a8d57ddd35a1e5c3087856395603ec
SENO <senokenya@senomac.local>
2021-04-17 13:10:32 +0900

    font

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,6 +40,7 @@ export default {
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
     '~plugins/scroll.js',
+    '~/plugins/adobe_fonts.js',
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
